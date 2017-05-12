10f92d3a1b589bd54f0c7783830c0427b025bab6
duanqz <duanqz@gmail.com>
2017-05-13 00:08:44 +0800

    Refine react

--- js/app.main.js
+++ js/app.main.js
@@ -868,7 +868,7 @@ var MailTableCore = React.createClass({
         </tr>}
         {this.props.tableData.map((data, index)=> {
             return (<tr key={data}>
-                <td>{data}</td>
+                <td style={Styles.centerText}>{data}</td>
                 <td style={{width: 86, textAlign: 'center'}}>
                     <button className="btn btn-link"
                             onClick={(event)=> {
